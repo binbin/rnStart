8620ab94e3d7e87e12a2f81b9f637716c6b17c6d
binbin <binbin5474@gmail.com>
2017-04-13 17:06:37 +0800

    index

--- index.android.js
+++ index.android.js
@@ -10,61 +10,95 @@ import {
   ListView,
   StyleSheet,
   Text,
+  Image,
   View, 
   Navigator, 
   TouchableHighlight ,
-  BackAndroid
+  TouchableOpacity,
+  BackAndroid,
+  AlertIOS
 } from 'react-native';
 
-import ListComponent from './components/ListComponent'
-import ContentComponent from './components/ContentComponent'
-
-var _navigator;
 
 
+// 获取屏幕宽度  
+var Dimensions = require('Dimensions');  
+const screenW = Dimensions.get('window').width;  
 
-export default class rnStart extends Component {
-  constructor(props) {
-  	super(props);
-  }
-  renderScene(route, navigator){
-  	_navigator = navigator
-  	if(route.id === 'list'){
-	    return (
-	      <ListComponent navigator={navigator} route={route} />
-	     );
+// 导入json数据  
+var shareData = require('./shareData.json');  
+  
+// 常量设置  
+const cols = 3;  
+const cellWH = 100;  
+const vMargin = (screenW - cellWH * cols) / (cols + 1);  
+const hMargin = 25;  
+
+
+
+
+
+export default class rnStart extends Component{
+	constructor(props) {
+		super(props);
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}); 
+    this.state = {
+      dataSource: ds.cloneWithRows(shareData),
+    }
 	}
-	if(route.id==='content'){
-		return (<ContentComponent/>)
+	render(){
+		return(  
+            <ListView  
+                dataSource={this.state.dataSource}  
+                renderRow={this.renderRow.bind(this)}  
+                contentContainerStyle={styles.listViewStyle}  
+            />  
+        )
 	}
-  }
-  render() {
-    return (
-	     <Navigator
-		  initialRoute={{ title: 'Main', id:'list'}}
-		  // configureScence={{ configureScence }}
-		  renderScene={this.renderScene}
-		/>
-    );
-  }
-  componentDidMount() {
- //  	BackAndroid.addEventListener('hardwareBackPress', function() {
-	//   if(_navigator == null){
-	//     return false;
-	//   }
-	//   if(_navigator.getCurrentRoutes().length === 1){
-	//     return false;
-	//   }
-	//   _navigator.pop();
-	//   return true;
-	// });
-  }
+	renderRow(rowData){  
+        return(  
+            <TouchableOpacity activeOpacity={0.8} onPress={()=>{AlertIOS.alert('点击了')}} >  
+                <View style={styles.innerViewStyle}>  
+                    <Image source={{uri:rowData.icon}} style={styles.iconStyle} />  
+                    <Text>{rowData.title}</Text>  
+                </View>  
+            </TouchableOpacity>  
+        );  
+    }
 }
 
-
-
+const styles = StyleSheet.create({  
+    listViewStyle:{  
+        // 主轴方向  
+        flexDirection:'row',  
+        // 一行显示不下,换一行  
+        flexWrap:'wrap',  
+        // 侧轴方向  
+        alignItems:'center', // 必须设置,否则换行不起作用  
+    },  
+  
+    innerViewStyle:{  
+        width:cellWH,  
+        height:cellWH,  
+        marginLeft:vMargin,  
+        marginTop:hMargin,  
+        // 文字内容居中对齐  
+        alignItems:'center'  
+    },  
+  
+    iconStyle:{  
+        width:80,  
+        height:80,  
+    },  
+  
+}); 
 
 
 
 AppRegistry.registerComponent('rnStart', () => rnStart);
+
+
+
+
+
 
