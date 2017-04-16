00ac51e1425b9cb32c04517d6f1806db4eec24b3
binbin <binbin5474@gmail.com>
2017-04-16 22:26:26 +0800

    *

---
+++ components/InitialView.js
@@ -0,0 +1,111 @@
+
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  ListView,
+  StyleSheet,
+  Text,
+  Image,
+  View, 
+  Navigator, 
+  TouchableHighlight ,
+  TouchableOpacity,
+  BackAndroid,
+  AlertIOS
+} from 'react-native';
+
+import NavigationBar from 'react-native-navbar';
+
+// 获取屏幕宽度  
+var Dimensions = require('Dimensions');  
+const screenW = Dimensions.get('window').width;  
+
+// 导入json数据  
+var shareData = require('./shareData.json');  
+  
+// 常量设置  
+const cols = 3;  
+const cellWH = 100;  
+const vMargin = (screenW - cellWH * cols) / (cols + 1);  
+const hMargin = 25;  
+
+
+shareData=[{
+	icon:'https://facebook.github.io/react/img/logo_og.png',
+	title:'xxx'
+}]
+
+
+export default class InitialView extends Component{
+	constructor(props) {
+		super(props);
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}); 
+	this.state = {
+	  dataSource: ds.cloneWithRows(shareData),
+	}
+	}
+	render(){
+		return(  
+			<View>
+			 <NavigationBar
+					title={titleConfig}
+					rightButton={rightButtonConfig}
+				  />
+			<ListView  
+				dataSource={this.state.dataSource}  
+				renderRow={this.renderRow.bind(this)}  
+				contentContainerStyle={styles.listViewStyle}  
+			/>  
+			</View>
+			
+		)
+	}
+	renderRow(rowData){  
+		return(<TouchableOpacity activeOpacity={0.8} onPress={()=>{AlertIOS.alert('点击了')}} >  
+				<View style={styles.innerViewStyle}>  
+					<Image source={{uri:rowData.icon}} style={styles.iconStyle} />  
+					<Text>{rowData.title}</Text>  
+				</View>  
+			</TouchableOpacity>  
+		);  
+	}
+}
+
+const rightButtonConfig = {
+  title: 'Next',
+  handler: () => alert('hello!'),
+};
+
+const titleConfig = {
+  title: 'Hello, world',
+};
+
+
+const styles = StyleSheet.create({  
+	listViewStyle:{  
+		// 主轴方向  
+		flexDirection:'row',  
+		// 一行显示不下,换一行  
+		flexWrap:'wrap',  
+		// 侧轴方向  
+		alignItems:'center', // 必须设置,否则换行不起作用  
+	},  
+  
+	innerViewStyle:{  
+		width:cellWH,  
+		height:cellWH,  
+		marginLeft:vMargin,  
+		marginTop:hMargin,  
+		// 文字内容居中对齐  
+		alignItems:'center'  
+	},  
+  
+	iconStyle:{  
+		width:80,  
+		height:80,  
+	},  
+  
+}); 
+
--- index.android.js
+++ index.android.js
@@ -21,18 +21,10 @@ import {
 
 
 
-// 获取屏幕宽度  
-var Dimensions = require('Dimensions');  
-const screenW = Dimensions.get('window').width;  
 
-// 导入json数据  
-var shareData = require('./shareData.json');  
-  
-// 常量设置  
-const cols = 3;  
-const cellWH = 100;  
-const vMargin = (screenW - cellWH * cols) / (cols + 1);  
-const hMargin = 25;  
+import InitialView from './components/InitialView'
+
+
 
 
 
@@ -41,58 +33,22 @@ const hMargin = 25;
 export default class rnStart extends Component{
 	constructor(props) {
 		super(props);
-		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}); 
-    this.state = {
-      dataSource: ds.cloneWithRows(shareData),
-    }
 	}
 	render(){
-		return(  
-            <ListView  
-                dataSource={this.state.dataSource}  
-                renderRow={this.renderRow.bind(this)}  
-                contentContainerStyle={styles.listViewStyle}  
-            />  
-        )
+		return (<Navigator
+                            style={styles.navigator}
+                            renderScene={this.renderScene.bind(this)}
+                            initialRoute={{ title: 'Main', id:'list'}}
+                          />
+                        );
 	}
-	renderRow(rowData){  
-        return(  
-            <TouchableOpacity activeOpacity={0.8} onPress={()=>{AlertIOS.alert('点击了')}} >  
-                <View style={styles.innerViewStyle}>  
-                    <Image source={{uri:rowData.icon}} style={styles.iconStyle} />  
-                    <Text>{rowData.title}</Text>  
-                </View>  
-            </TouchableOpacity>  
-        );  
-    }
+	renderScene(route, navigator) {    
+        return (<InitialView navigator={navigator} route={route} />)
+      }
 }
-
-const styles = StyleSheet.create({  
-    listViewStyle:{  
-        // 主轴方向  
-        flexDirection:'row',  
-        // 一行显示不下,换一行  
-        flexWrap:'wrap',  
-        // 侧轴方向  
-        alignItems:'center', // 必须设置,否则换行不起作用  
-    },  
-  
-    innerViewStyle:{  
-        width:cellWH,  
-        height:cellWH,  
-        marginLeft:vMargin,  
-        marginTop:hMargin,  
-        // 文字内容居中对齐  
-        alignItems:'center'  
-    },  
-  
-    iconStyle:{  
-        width:80,  
-        height:80,  
-    },  
-  
-}); 
-
+const styles = StyleSheet.create({
+  navigator:{}
+})
 
 
 AppRegistry.registerComponent('rnStart', () => rnStart);
